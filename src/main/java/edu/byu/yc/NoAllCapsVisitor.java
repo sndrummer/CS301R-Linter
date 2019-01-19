fbefa41b97351176f7a8bbc6824eadea376986f7
Samuel Nuttall <sndrummer@gmail.com>
2019-01-18 23:03:39 -0700

    added project

---
+++ src/main/java/edu/byu/yc/NoAllCapsVisitor.java
@@ -0,0 +1,36 @@
+package edu.byu.yc;
+
+/**
+ * @author Samuel Nuttall
+ */
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.SimpleName;
+
+/**
+ * @author
+ */
+public class NoAllCapsVisitor extends ASTVisitor {
+
+    public NoAllCapsVisitor() {
+        allCaps = new HashSet<>();
+    }
+
+    private Set<String> allCaps;
+
+    @Override
+    public boolean visit(SimpleName sn) {
+        String name = sn.toString();
+        if (name == name.toUpperCase()) {
+            allCaps.add(name);
+        }
+        return true;
+    }
+
+    public Set<String> getAllCaps() {
+        return allCaps;
+    }
+}
---
+++ src/main/java/edu/byu/yc/Poop.java
@@ -0,0 +1,15 @@
+package edu.byu.yc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Samuel Nuttall
+ */
+public class Poop {
+    private static Logger logger = LoggerFactory.getLogger(Poop.class);
+
+    public static void main(String[] args){
+        logger.info("I hate my life");
+    }
+}
